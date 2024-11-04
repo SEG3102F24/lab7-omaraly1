76ea307c463ce8824bdd77bdae09385515c6208e
omaraly1 <oaly042@uottawa.ca>
2024-11-03 23:45:18 -0500

    lab7

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,29 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {AboutComponent} from "./about/about.component";
-import {ContactComponent} from "./contact/contact.component";
-import {BooksComponent} from "./books/books.component";
-import {BookComponent} from "./books/book/book.component";
-import {LoginComponent} from "./login/login.component";
-import {AdminComponent} from "./admin/admin.component";
-import {loggedInGuard} from "./logged-in.guard";
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BooksComponent } from './books/books.component';
+import { BookComponent } from './books/book/book.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { loggedInGuard } from './logged-in.guard';
+import { authorRoutes, AuthorsComponent } from './authors/authors.component';
 
-const booksRoutes: Routes = [
-  {path: ':id', component: BookComponent}
-];
+const booksRoutes: Routes = [{ path: ':id', component: BookComponent }];
 
 export const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'contact', component: ContactComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'authors', component: AuthorsComponent },
   { path: 'login', component: LoginComponent },
   {
     path: 'admin',
     component: AdminComponent,
-    canActivate: [ loggedInGuard ]
+    canActivate: [loggedInGuard],
   },
-  {path: 'books', component: BooksComponent,
-    children: booksRoutes
-  },
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: '**', component: HomeComponent}
+  { path: 'books', component: BooksComponent, children: booksRoutes },
+  { path: 'authors', component: AuthorsComponent, children: authorRoutes },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent },
 ];
